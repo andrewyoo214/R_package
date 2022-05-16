b35e032029aab8f533c3f62d673cd8a2a2756ddc
andrewyoo <andrewyoo@g.skku.edu>
2022-05-16 20:13:12 +0900

    R package practices

---
+++ R/mean.R
@@ -0,0 +1,22 @@
+
+
+#' @title Mean_function_with_narm
+#' @description FUNCTION_DESCRIPTION
+#' @param x PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples 
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @rdname mean0
+#' @export 
+
+mean0 <- function(x){
+  mean(x, na.rm = T)
+}
+
+
+
--- package_generation.R
+++ package_generation.R
@@ -2,7 +2,19 @@ install.packages("remotes")
 library(remotes)
 
 remotes::install_github("andrewyoo214/R_package")
-library(RTHON2020)
+library(pkgandrew)
+
+usethis::use_data(mtcars)
 
 
+install.packages('sinew')
+install.packages("devtools")
+devtools::document()  ## documentation 생성을 확인
+devtools::check()     ## 잘 만들어져 있는지 확인
+
+
+pkgandrew::mtcars
+
+
+rm(mean0)
 
